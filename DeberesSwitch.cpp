435e5ead0a7b7ea418b7b31403b2a432fccc6690
helbramn <barabasrori@gmail.com>
2024-10-14 21:58:49 +0200

    Agregar archivos de proyecto.

---
+++ DeberesSwitch.cpp
@@ -0,0 +1,36 @@
+﻿#include "DeberesSwitch.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int edad;
+	bool licencia;
+
+	cout << "Ingrese su edad: ";
+	cin >> edad;
+
+	switch (edad >= 18) {
+
+	case 0:
+		cout << "no puede conducir siendo menor";
+		break;
+
+	case 1:
+	  cout << "Tiene licencia de conducir? (1 si, 0 no) ";
+	  cin >> licencia;
+		switch (licencia) {
+		case false:
+			cout << "no puedes conducir sin licencia";
+			break;
+		case true:
+			cout << "puede conducir sin problema";
+			break;
+		default:
+			cout << "ese valor no es legible, responda con claridad";
+			break;
+		}
+		break;
+	}
+
+}
